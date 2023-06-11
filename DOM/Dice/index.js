250155ae8c235281209c3a03e5fcd16f37571553
Aldor Tran <aldor.tran@gmail.com>
2023-06-11 15:58:28 +0700

    update

---
+++ DOM/Dice/index.js
@@ -0,0 +1,2 @@
+var randomNumber1 = Math.random * 7;
+console.log(randomNumber1)
---
+++ DOM/index.js
@@ -0,0 +1,6 @@
+// document.querySelector('.Third').innerHTML = "Levi";
+
+// document.getElementsByTagName('li')[0].style.color = "red";
+
+document.querySelectorAll('li')[0].style.color = "red"; //chọn tất cả
+document.querySelector('button').style.backgroundColor = "red"
